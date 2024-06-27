989d6ee6ef4619669a0012bacf9634cdb5f90d3d
Sigmund Granaas Sandring <46557903+SigmundGranaas@users.noreply.github.com>
2024-06-27 07:48:09 +0200

    [Patch]: Dynamic GKT fetcher for satellite images (#10)
    Dynamically fetches a new GKT key every hour

--- lib/tile_providers.dart
+++ lib/tile_providers.dart
@@ -3,7 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:proj4dart/proj4dart.dart' as proj4;
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 
 TileLayer get openStreetMapTileLayer => TileLayer(
@@ -20,19 +21,24 @@ TileLayer get norgesKart => TileLayer(
   tileProvider: CancellableNetworkTileProvider(),
 );
 
-TileLayer get norgesKartSatelitt => TileLayer(
-  tileProvider: CustomNorwayTileProvider(),
+TileLayer norgesKartSatelitt(String gkt) => TileLayer(
+  tileProvider: CustomNorwayTileProvider(gkt),
 );
 
 
 class CustomNorwayTileProvider extends TileProvider {
   final String baseUrl = 'https://gatekeeper1.geonorge.no/BaatGatekeeper/gk/gk.nib_web_mercator_wmts_v2';
 
+  final String gkt;
+
+  CustomNorwayTileProvider(this.gkt);
+
+
   @override
   ImageProvider getImage(TileCoordinates coordinates, TileLayer options) {
     final url = '$baseUrl?'
         'layer=Nibcache_web_mercator_v2'
-        '&gkt=318F5FCE232B94954B1329985CB33E700322D0E71099F30E0D7733B79E21E0BBC1AD0D4B4F2857DAE7C1F3CF9034009318B855F07FC2AB828D018F0853CD0DA1'
+        '&gkt=$gkt'
         '&style=default'
         '&tilematrixset=GoogleMapsCompatible'
         '&Service=WMTS'
@@ -43,5 +49,38 @@ class CustomNorwayTileProvider extends TileProvider {
         '&TileCol=${coordinates.x}'
         '&TileRow=${coordinates.y}';
     return NetworkImage(url, headers: {'User-Agent': 'app/1.0'});
+  }
+}
+
+class GktManager {
+  static final GktManager _instance = GktManager._internal();
+  factory GktManager() => _instance;
+  GktManager._internal();
+
+  String? _gkt;
+  DateTime? _lastFetchTime;
+
+  Future<String> getGkt() async {
+    if (_gkt != null && _lastFetchTime != null &&
+        DateTime.now().difference(_lastFetchTime!).inHours < 1) {
+      return _gkt!;
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse('https://norgeskart.no/ws/gatekeeper.py?key=73e029c3632c49bb1586fc57a60fb701kv'),
+        headers: {'User-Agent': 'app/1.0'},
+      ).timeout(const Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        _gkt = response.body.trim().replaceAll('"', '');
+        _lastFetchTime = DateTime.now();
+        return _gkt!;
+      } else {
+        throw Exception('Failed to load GKT code');
+      }
+    } catch (e) {
+      throw Exception('Error fetching GKT code: $e');
+    }
   }
 }
