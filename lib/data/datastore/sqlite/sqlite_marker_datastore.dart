40ae3e9e099ee7b36491b2c50c1acd51aee7e760
sigmund <sigmundsgranaas@gmail.com>
2025-06-12 07:22:13 +0200

    * Fixed tests

--- lib/data/datastore/sqlite/sqlite_marker_datastore.dart
+++ lib/data/datastore/sqlite/sqlite_marker_datastore.dart
@@ -42,6 +42,28 @@ class SQLiteMarkerDataStore implements MarkerDataStore {
     );
   }
 
+  @visibleForTesting
+  void injectDatabase(Database db) {
+    _db = db;
+  }
+
+  @visibleForTesting
+  Future<void> createTable(Database db) async {
+    await db.execute('''
+      CREATE TABLE $_tableName(
+        uuid TEXT PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        icon TEXT,
+        latitude REAL NOT NULL,
+        longitude REAL NOT NULL,
+        synced INTEGER NOT NULL DEFAULT 0
+      )
+    ''');
+    await db.execute('CREATE INDEX idx_markers_coords ON $_tableName(latitude, longitude)');
+    await db.execute('CREATE INDEX idx_markers_synced ON $_tableName(synced)');
+  }
+
   @override
   Future<void> init() async {
     await database; // Ensures DB is initialized
